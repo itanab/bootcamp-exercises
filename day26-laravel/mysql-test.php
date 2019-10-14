b55bdb4256afa13e8fb1adffc3afb39768450198
itana <itanabulatovic.ib@gmail.com>
2019-10-14 17:13:13 +0200

    mysql

--- day26-laravel/mysql-test.php
+++ day26-laravel/mysql-test.php
@@ -3,6 +3,7 @@
 require_once 'DB/DB.php';
 require_once 'DB/DB_functions.php';
 require_once 'Country.php';
+require_once 'Region.php';
 
 //connect to a database
 connect('localhost', 'world','root', 'rootroot');
@@ -19,5 +20,29 @@ $results=select($query, [20000000], 'Country');
     return $country->getFormattedPopulation();
 }, $results);*/
 
-var_dump($results);
+//var_dump($results);
 //var_dump($populations);
+
+//$region = new Region;
+//$region->name = 'Central Africa';
+//$region->insert();
+
+//echo 'A new region was inserted with id'.$region->id.'<br>';
+
+
+//update the slug of Central Afrika
+
+$query = "
+    SELECT *
+    FROM `region`
+    WHERE `name` = ?
+";
+
+$central_africa = select_one($query, ['Central Africa'], 'Region');
+
+
+
+$central_africa->slug = 'central-africa';
+$central_africa->update();
+
+var_dump($central_africa);
